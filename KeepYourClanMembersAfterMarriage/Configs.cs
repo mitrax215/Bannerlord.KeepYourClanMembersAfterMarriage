33586ef8db5161acd722b815bb0504cd7d918786
MitraX <mitrax215@gmail.com>
2025-12-09 18:15:57 +0300

    feat: keep clan members after marriage

--- KeepYourClanMembersAfterMarriage/Configs.cs
+++ KeepYourClanMembersAfterMarriage/Configs.cs
@@ -1,4 +1,6 @@
-﻿using MCM.Abstractions.Base.Global;
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
 
 namespace KeepYourClanMembersAfterMarriage
 {
@@ -11,5 +13,11 @@ namespace KeepYourClanMembersAfterMarriage
         public override string FolderName => "KeepYourClanMembersAfterMarriage";
 
         public override string FormatType => "json2";
+
+        private const string HeadingKeepClanMembersAfterMarriage = "Keep Your Clan Members After Marriage";
+
+        [SettingPropertyBool(displayName: "Keep clan members", Order = 1, RequireRestart = false, HintText = "Allow a clan member to remain in your clan after they get married.")]
+        [SettingPropertyGroup(HeadingKeepClanMembersAfterMarriage)]
+        public bool KeepClanMembersAfterMarriage { get; set; } = true;
     }
 }
---
+++ KeepYourClanMembersAfterMarriage/Patches/DefaultMarriageModelPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+
+namespace KeepYourClanMembersAfterMarriage.Patches
+{
+    [HarmonyPatch(typeof(DefaultMarriageModel))]
+    class DefaultMarriageModelPatch
+    {
+        private static readonly bool KeepClanMembersAfterMarriage = Configs.Instance.KeepClanMembersAfterMarriage;
+
+        [HarmonyPrefix]
+        [HarmonyPatch("GetClanAfterMarriage")]
+        public static bool GetClanAfterMarriage(Hero firstHero, Hero secondHero, ref Clan __result)
+        {
+            if (!KeepClanMembersAfterMarriage)
+            {
+                return true;
+            }
+
+            if (firstHero.Clan == Hero.MainHero.Clan)
+            {
+                __result = firstHero.Clan;
+
+                return false;
+            }
+            else if (secondHero.Clan == Hero.MainHero.Clan)
+            {
+                __result = secondHero.Clan;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
