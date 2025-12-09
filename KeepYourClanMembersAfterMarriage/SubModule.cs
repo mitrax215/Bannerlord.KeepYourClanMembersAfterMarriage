b4b54cb622f871b88854845cb8889a2e3fae3e32
MitraX <mitrax215@gmail.com>
2025-12-09 16:16:17 +0300

    Initial commit

---
+++ KeepYourClanMembersAfterMarriage/SubModule.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+
+namespace KeepYourClanMembersAfterMarriage
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            Harmony harmony = new Harmony("com.keepyourclanmembersaftermarriage");
+
+            harmony.PatchAll();
+        }
+    }
+}
